c66db1f6bcc7499cfb2f77216e93ba14024f976b
Veronika <maskaarhangela@gmail.com>
2025-05-12 19:32:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-import cors from "cors";
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { router as fighterRoutes } from "./routes/fighterRoutes.js";
+import { responseMiddleware } from "./middlewares/response.middleware.js";
 import { initRoutes } from "./routes/routes.js";
-
 import "./config/db.js";
 
 const app = express();
+const PORT = 3000;
 
+// Middleware setup
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(responseMiddleware);
 
+// Routes setup
+app.use("/api/fighters", fighterRoutes);
 initRoutes(app);
 
+// Serve static files (if needed)
 app.use("/", express.static("./client/build"));
 
-const port = 3050;
-app.listen(port, () => {});
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
 export { app };
