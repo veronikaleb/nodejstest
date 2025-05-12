e49b420b823e0e0466a1688926b65973f75db129
Veronika <maskaarhangela@gmail.com>
2025-05-12 19:41:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,17 @@ import "./config/db.js";
 const app = express();
 const PORT = 3000;
 
-// Middleware setup
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(responseMiddleware);
 
-// Routes setup
 app.use("/api/fighters", fighterRoutes);
 initRoutes(app);
 
-// Serve static files (if needed)
 app.use("/", express.static("./client/build"));
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
