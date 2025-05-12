1d639385f488e4acfb50878d858c9377fa8b42e3
Veronika <maskaarhangela@gmail.com>
2025-05-12 19:26:25 +0300

    Update fighterRepository.js

--- repositories/fighterRepository.js
+++ repositories/fighterRepository.js
@@ -1,8 +1,12 @@
-import { BaseRepository } from "./baseRepository.js";
+import { BaseRepository } from './baseRepository.js';
 
 class FighterRepository extends BaseRepository {
   constructor() {
-    super("fighters");
+    super('fighters');
+  }
+
+  findByName(name) {
+    return this.getAll().find(fighter => fighter.name.toLowerCase() === name.toLowerCase());
   }
 }
 
