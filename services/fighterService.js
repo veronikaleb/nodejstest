8041954d3d104343b6938d2fa7c9688697880ebc
Veronika <maskaarhangela@gmail.com>
2025-05-12 19:34:02 +0300

    Update fighterService.js

--- services/fighterService.js
+++ services/fighterService.js
@@ -1,9 +1,30 @@
-import { fighterRepository } from "../repositories/fighterRepository.js";
+import { v4 as uuid } from 'uuid';
+import { FIGHTER } from "../models/fighter.js";
+import { FighterRepository } from "../repositories/fighterRepository.js";
 
-class FighterService {
-  // TODO: Implement methods to work with fighters
-}
+const fighterRepo = new FighterRepository();
 
-const fighterService = new FighterService();
+export const fighterService = {
+  getAll: () => fighterRepo.getAll(),
+  getById: (id) => fighterRepo.getById(id),
+  
+  create: (fighterData) => {
+    if (fighterRepo.findByName(fighterData.name)) {
+      throw new Error("Fighter with this name already exists");
+    }
+    const newFighter = { ...fighterData, id: uuid(), health: fighterData.health || 85 };
+    return fighterRepo.create(newFighter);
+  },
 
-export { fighterService };
+  update: (id, updatedData) => {
+    const fighter = fighterRepo.getById(id);
+    if (!fighter) throw new Error("Fighter not found");
+    return fighterRepo.update(id, updatedData);
+  },
+
+  delete: (id) => {
+    const fighter = fighterRepo.delete(id);
+    if (!fighter) throw new Error("Fighter not found");
+    return fighter;
+  }
+};
