39407e0eba4c8f3b2bcf10d331ba54dab4d80040
Veronika <maskaarhangela@gmail.com>
2025-05-12 19:36:59 +0300

    Update fightRoutes.js

--- routes/fightRoutes.js
+++ routes/fightRoutes.js
@@ -1,13 +1,26 @@
 import { Router } from "express";
 import { fightersService } from "../services/fightService.js";
-import {
-  createUserValid,
-  updateUserValid,
-} from "../middlewares/user.validation.middleware.js";
 import { responseMiddleware } from "../middlewares/response.middleware.js";
 
 const router = Router();
 
-// OPTIONAL TODO: Implement route controller for fights
+router.post("/", async (req, res) => {
+  try {
+    const fightResult = await fightersService.startFight(req.body);
+    res.sendResponse(fightResult);
+  } catch (err) {
+    res.sendError(err.message);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const fightHistory = await fightersService.getFightHistory();
+    res.sendResponse(fightHistory);
+  } catch (err) {
+    res.sendError(err.message);
+  }
+});
 
 export { router };
+
