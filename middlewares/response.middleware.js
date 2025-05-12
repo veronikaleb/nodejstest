b0f8eda8e2613f4cf46ae7fb02ef42941db1e9f4
Veronika <maskaarhangela@gmail.com>
2025-05-12 19:38:50 +0300

    Update response.middleware.js

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,6 +1,14 @@
 const responseMiddleware = (req, res, next) => {
-  // TODO: Implement middleware that returns result of the query
+  if (res.data) {
+    return res.status(200).json(res.data);
+  }
+  
+  if (res.err) {
+    return res.status(400).json({ error: true, message: res.err.message });
+  }
+
   next();
 };
 
 export { responseMiddleware };
+
